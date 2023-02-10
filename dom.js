9ceb5a40f30e4e1406fd7ff90821727c070fac69
Dragan Vekic <kikivekic999@gmail.com>
2023-02-10 15:53:36 +0100

    dodali selektore

--- dom.js
+++ dom.js
@@ -24,3 +24,44 @@ for(let i=0; i<items.length; i++){
     if(i%2==0) items[i].style.backgroundColor="grey";
     else items[i].style.backgroundColor= "lightgrey";
 }
+
+console.dir(document.getElementById("header-title"))
+let headerTitle=document.getElementById("header-title");
+headerTitle.innerHTML="<h3> ASD </h3>";
+console.log(headerTitle.textContent)
+console.log(headerTitle.innerText)
+
+// textcontent vs innertext
+
+console.log(document.getElementsByTagName("li"));
+let li=document.getElementsByTagName("li");
+li[1].textContent="asd";
+li[1].style.fontSize="20px";
+li[1].style.fontWeight="bold";
+
+
+
+let title=document.getElementsByClassName("title")
+title[1].textContent="Jabuka"
+
+
+let container=document.getElementById("container")
+container.style.backgroundColor="purple"
+
+//Queryselector
+
+let Drugilink=document.querySelector(".list-item")
+console.log(Drugilink);
+
+Drugilink.style.backgroundColor="red"
+
+let p= document.querySelector("svejedno")
+psvejedno[0].style.backgroundColor="blue"
+
+//QueryslectorAll
+
+let liste=document.querySelectorAll(".list-item")
+console.log(lista);
+
+lista[2].textContent=" ne znam sta"
+
